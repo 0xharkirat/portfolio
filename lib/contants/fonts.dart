f9359bab299b18c27a55eefdf898a41878b69d4a
Harkirat Singh <sandhukirat23@gmail.com>
2023-12-24 02:06:50 +1100

    main hero

--- lib/contants/fonts.dart
+++ lib/contants/fonts.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/contants/colors.dart';
+
 const kAcornFonts = "Acorn";
+
+
+TextStyle headlineStyle = const TextStyle(
+  fontSize: 96.0,
+  fontWeight: FontWeight.bold,
+  color: kTextColor,
+  fontFamily: kAcornFonts
+  // You can add more styling properties as needed
+);
+
+
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/contants/colors.dart';
+import 'package:portfolio/contants/fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
       body: Center(
-        child: Text("This is home Page"),
-      ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: [
+            
+            Text("Hi. I am Harkirat.", style: headlineStyle,),
+            Text("A Developer.", style: headlineStyle,)
+          ],
+        ),
+      )
     );
   }
 }
