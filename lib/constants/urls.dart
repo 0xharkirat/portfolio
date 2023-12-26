c342e55c1922e8dc1f9d7230d08c26feac04f5e3
Harkirat Singh <sandhukirat23@gmail.com>
2023-12-27 03:24:13 +1100

    url about

---
+++ lib/constants/urls.dart
@@ -0,0 +1,2 @@
+const aboutUrl =
+    "https://hushed-boat-f59.notion.site/Harkirat-Singh-6d452426417d45abab18966092930cf6";
--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: avoid_web_libraries_in_flutter
+
 import 'package:flutter/material.dart';
 import 'package:portfolio/constants/fonts.dart';
+import 'package:portfolio/constants/urls.dart';
 import 'package:transparent_image/transparent_image.dart';
+import 'dart:js' as js;
 
 class AboutPage extends StatelessWidget {
   const AboutPage({Key? key}) : super(key: key);
@@ -23,45 +27,50 @@ class AboutPage extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 72),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: size.width * 0.04),
-              padding: const EdgeInsets.fromLTRB(20, 40, 20, 0),
-              height: size.height,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: Colors.white30, width: 2),
-                color: Colors.black54,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.3),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                    offset: const Offset(0, 2),
-                  ),
-                ],
-              ),
+            InkWell(
+              onTap: () {
+                js.context.callMethod('open', [aboutUrl]);
+              },
               child: Container(
-                decoration: const BoxDecoration(
-                  
-                  border: Border(
-                      top: BorderSide(color: Colors.white30, width: 2),
-                      left: BorderSide(color: Colors.white30, width: 2),
-                      right: BorderSide(
-                        color: Colors.white30,
-                        width: 2,
-                      )),
+                margin: EdgeInsets.symmetric(horizontal: size.width * 0.04),
+                padding: const EdgeInsets.fromLTRB(20, 40, 20, 0),
+                height: size.height,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.white30, width: 2),
+                  color: Colors.black54,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.3),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
                 ),
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    
+                    border: Border(
+                        top: BorderSide(color: Colors.white30, width: 2),
+                        left: BorderSide(color: Colors.white30, width: 2),
+                        right: BorderSide(
+                          color: Colors.white30,
+                          width: 2,
+                        )),
                   ),
-                  child: FadeInImage.memoryNetwork(
-                    placeholder: kTransparentImage,
-                    image: "assets/images/about.png",
-                    fit: BoxFit.fitWidth,
-                    alignment: Alignment.topCenter,
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                    ),
+                    child: FadeInImage.memoryNetwork(
+                      placeholder: kTransparentImage,
+                      image: "assets/images/about.png",
+                      fit: BoxFit.fitWidth,
+                      alignment: Alignment.topCenter,
+                    ),
                   ),
                 ),
               ),
