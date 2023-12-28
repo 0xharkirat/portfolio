6b0fe82a36c96cf92fe34d8ada169d9925ae6b16
Harkirat Singh <sandhukirat23@gmail.com>
2023-12-28 23:11:12 +1100

    page transition

--- lib/routes/route_generator.dart
+++ lib/routes/route_generator.dart
@@ -9,24 +9,26 @@ class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case routeHome:
-        return createRoute(const HomePage());
+        return createRoute(const HomePage(), settings);
 
       case routeAbout:
-        return createRoute(const AboutPage());
+        return createRoute(const AboutPage(), settings);
       case routeProjects:
-        return createRoute(const ProjectPage());
+        return createRoute(const ProjectPage(), settings);
       default:
-        return createRoute(const ErrorPage());
+        return createRoute(const ErrorPage(), settings);
     }
   }
 
-  static createRoute(Widget route) {
+  static createRoute(Widget route, RouteSettings settings) {
   return PageRouteBuilder(
+    settings: settings,
+    transitionDuration: const Duration(milliseconds: 690),
     pageBuilder: (context, animation, secondaryAnimation) => route,
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
       
       return FadeTransition(
-        opacity: CurveTween(curve: Curves.linear).animate(animation),
+        opacity: CurveTween(curve: Curves.easeInOutQuad).animate(animation),
         child: child,
       );
     },
