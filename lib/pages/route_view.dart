217eee14364cd09a489a669f56a95f0a60e54be9
Harkirat Singh <sandhukirat23@gmail.com>
2024-01-06 21:17:45 +1100

    Revert "added cursor blend mode"
    This reverts commit 158f0d0a66b7c99b3e71efe6038bdeba2fd6be9a.

--- lib/pages/route_view.dart
+++ lib/pages/route_view.dart
@@ -1,33 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/cursor_animation/animated_cursor.dart';
 import 'package:portfolio/widgets/navigation_bar.dart';
 
-class RouteView extends StatefulWidget {
+class RouteView extends StatelessWidget {
   const RouteView({super.key, required this.child});
   final Widget child;
-
-  @override
-  State<RouteView> createState() => _RouteViewState();
-}
-
-class _RouteViewState extends State<RouteView>
-    with SingleTickerProviderStateMixin {
-  late Offset? pointerOffset;
-
-  late AnimationController pointerSizeController;
-
-  late Animation<double> pointerAnimation;
-
-  @override
-  void initState() {
-    pointerOffset = const Offset(0, 0);
-    pointerSizeController = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 300));
-    pointerAnimation = CurvedAnimation(
-        curve: Curves.easeInOutCubic,
-        parent: Tween<double>(begin: 0, end: 1).animate(pointerSizeController));
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,46 +11,17 @@ class _RouteViewState extends State<RouteView>
       backgroundColor: Colors.transparent,
       body: Padding(
         padding: const EdgeInsets.all(31),
-        child: MouseRegion(
-          opaque: false,
-          cursor: SystemMouseCursors.none,
-          onHover: (event) {
-            setState(() {
-              pointerOffset = event.localPosition;
-            });
-          },
-          onExit: (event) {
-            setState(() {
-              pointerOffset = null;
-            });
-          },
-          child: Stack(
-            alignment: Alignment.centerLeft,
-            clipBehavior: Clip.none,
-            children: [
-              widget.child,
-              const Positioned(
-                left: 0,
-                child: NavigationBarWeb(),
-              ),
-              if (pointerOffset != null) ...[
-                AnimatedBuilder(
-                  animation: pointerSizeController,
-                  builder: (context, child) {
-                    return AnimatedPointer(
-                      pointerOffset: pointerOffset!,
-                      radius: 45 + 100 * pointerAnimation.value,
-                    );
-                  },
-                ),
-                AnimatedPointer(
-                  pointerOffset: pointerOffset!,
-                  movementDuration: const Duration(milliseconds: 200),
-                  radius: 10,
-                )
-              ],
-            ],
-          ),
+        child: Stack(
+          alignment: Alignment.centerLeft,
+          
+          clipBehavior: Clip.none,
+          children: [
+            child,
+            const Positioned(
+              left: 0,
+              child: NavigationBarWeb(),),
+            
+          ],
         ),
       ),
     );
--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
